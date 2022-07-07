e995d01f5dff90f59afa44840ea16335cbcc3350
hungpidev <hungn27091999@gmail.com>
2022-07-07 14:54:24 +0700

    update

--- app/js/toggle-menu.js
+++ app/js/toggle-menu.js
@@ -7,7 +7,11 @@ window.addEventListener("load", function () {
   }
   window.addEventListener("click", handleClickOutside);
   function handleClickOutside(e) {
-    if (e.target.matches(":not(.header__toggle,.menu,.menu *)")) {
+    if (
+      !e.target.matches(
+        ".toggle__label,.toggle__input,.toggle__slider,.header__toggle,.menu,.menu *"
+      )
+    ) {
       menu.classList.remove("is-show");
     }
   }
